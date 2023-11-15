139c6d270a8d306565907a522fef4f9574e3d4f6
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-16 04:16:52 +0530

    hook up login & register forms

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,55 @@
+"use client";
+
+import useUser from "@/app/hooks/useUser";
+import axios from "axios";
+import Link from "next/link";
+import { redirect, useRouter } from "next/navigation";
+import { useRef } from "react";
+// import axios from "@/app/config/axios.config";
+
 export default function LoginPage() {
+    const router = useRouter();
+    const { user, refetch } = useUser();
+
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    async function loginHandler() {
+        try {
+            const res = await axios.post(
+                "http://localhost:3002/login",
+                {
+                    username: usernameRef.current?.value,
+                    password: passwordRef.current?.value,
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    withCredentials: true,
+                }
+            );
+
+            console.log(res.data);
+            refetch();
+            router.push('/home');
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    if (user?.isLoggedIn) {
+        redirect('/home');
+    }
+
     return (
         <>
-            <a href="/register" className="absolute right-4 top-4 md:right-8 md:top-8">Signup</a>
+            <Link
+                href="/register"
+                className="absolute right-4 top-4 md:right-8 md:top-8"
+            >
+                Signup
+            </Link>
             <div className="mx-auto h-full flex w-full flex-col justify-center space-y-6">
                 <div className="flex flex-col space-y-2 text-center">
                     <h1 className="text-2xl font-semibold tracking-tight">
@@ -12,29 +60,42 @@ export default function LoginPage() {
                     </p>
                 </div>
                 <div>
-                    <form>
-                        <input name="username" type="text" />
-                        <input name="password" type="password" />
+                    <form className="flex flex-col gap-y-2 w-[400px] m-auto items-center">
+                        <input
+                            ref={usernameRef}
+                            placeholder="Username"
+                            name="username"
+                            type="text"
+                        />
+                        <input
+                            ref={passwordRef}
+                            placeholder="Password"
+                            name="password"
+                            type="password"
+                        />
+                        <button type="button" onClick={() => loginHandler()}>
+                            Log in
+                        </button>
                     </form>
                 </div>
                 <p className="px-8 text-center text-sm text-muted-foreground">
                     By clicking continue, you agree to our{" "}
-                    <a
+                    <Link
                         href="/terms"
                         className="underline underline-offset-4 hover:text-primary"
                     >
                         Terms of Service
-                    </a>{" "}
+                    </Link>{" "}
                     and{" "}
-                    <a
+                    <Link
                         href="/privacy"
                         className="underline underline-offset-4 hover:text-primary"
                     >
                         Privacy Policy
-                    </a>
+                    </Link>
                     .
                 </p>
             </div>
         </>
-    )
+    );
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+
+import axios from "axios";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+// import axios from "../../config/axios.config";
+import { useRef } from "react";
+import useUser from "@/app/hooks/useUser";
+
 export default function RegisterPage() {
+    const { user, refetch } = useUser();
+
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    async function registerHandler() {
+        try {
+            const res = await axios.post("http://localhost:3002/register", {
+                username: usernameRef.current?.value,
+                email: emailRef.current?.value,
+                password: passwordRef.current?.value,
+            });
+
+            console.log(res.data);
+            redirect('/login');
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    if (user?.isLoggedIn) {
+        redirect('/home');
+    }
+
     return (
         <>
-            <a href="/login" className="absolute right-4 top-4 md:right-8 md:top-8">Signup</a>
+            <Link href="/login" className="absolute right-4 top-4 md:right-8 md:top-8">
+                Log in
+            </Link>
             <div className="mx-auto h-full flex w-full flex-col justify-center space-y-6">
                 <div className="flex flex-col space-y-2 text-center">
                     <h1 className="text-2xl font-semibold tracking-tight">
@@ -12,29 +48,33 @@ export default function RegisterPage() {
                     </p>
                 </div>
                 <div>
-                    <form>
-                        <input name="username" type="text" />
-                        <input name="password" type="password" />
+                    <form className="flex flex-col gap-y-2">
+                        <input ref={usernameRef} placeholder="username" name="username" type="text" />
+                        <input ref={emailRef} placeholder="email" name="email" type="email" />
+                        <input ref={passwordRef} placeholder="password" name="password" type="password" />
+                        <button type="button" onClick={() => registerHandler()}>
+                            Submit
+                        </button>
                     </form>
                 </div>
                 <p className="px-8 text-center text-sm text-muted-foreground">
                     By clicking continue, you agree to our{" "}
-                    <a
+                    <Link
                         href="/terms"
                         className="underline underline-offset-4 hover:text-primary"
                     >
                         Terms of Service
-                    </a>{" "}
+                    </Link>{" "}
                     and{" "}
-                    <a
+                    <Link
                         href="/privacy"
                         className="underline underline-offset-4 hover:text-primary"
                     >
                         Privacy Policy
-                    </a>
+                    </Link>
                     .
                 </p>
             </div>
         </>
-    )
+    );
 }
