c82d273759c61657fcc584374cf472418a534bd5
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-19 23:30:50 +0530

    more ui styling

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import JoinDiscordButton from "../components/JoinDiscordButton"
+import Logo from '../components/Logo';
 
 export default function Layout({
     children,
@@ -9,8 +10,7 @@ export default function Layout({
         <>
             <div className="flex h-screen">
                 <div className="bg-zinc-900 container relative hidden flex-col items-center justify-center md:grid lg:max-w-none lg:px-0">
-                    <h2 className="absolute left-4 top-4 md:left-8 md:top-8">BitWarriors</h2>
-                    <h1 className="text-center">Crafted with love by Alan</h1>
+                    <h2 className="absolute font-mono left-4 top-4 md:left-8 md:top-8"><Logo /></h2>
                     <JoinDiscordButton />
                 </div>
                 <div className="bg-zinc-800 relative lg:p-8 w-100 sm:min-w-[650px]">
---
+++ src/app/components/Logo.tsx
@@ -0,0 +1,7 @@
+const Logo = () => {
+  return (
+    <>{"<Bit"}<span className="text-primary font-extrabold">{"Warriors"}</span>{"/>"}</>
+  )
+}
+
+export default Logo;
