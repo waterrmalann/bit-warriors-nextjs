a1fe6fff65de4ea2a4cc64e669240219dba427ae
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-27 10:07:54 +0530

    added `success` and `warning`

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -30,6 +30,8 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
+          success: "success group border-green-500 bg-green-500 text-neutral-50",
+          warning: "warning group border-amber-700 bg-amber-700 text-neutral-50",
       },
     },
     defaultVariants: {
