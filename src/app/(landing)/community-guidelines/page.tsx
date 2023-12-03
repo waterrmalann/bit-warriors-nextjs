0fc51678534cf9447ddb60f566e86b62ad6c2534
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-12-03 23:17:26 +0530

    feat(page): add community guidelines

---
+++ src/app/(landing)/community-guidelines/page.tsx
@@ -0,0 +1,39 @@
+function CommunityGuidelines() {
+    return (
+        <div className="mx-auto max-w-5xl p-4">
+            <h1 className="text-4xl font-bold mb-4">Community Guidelines</h1>
+            <p className="mb-4">Welcome to <strong>{"<BitWarriors/>"}</strong>! Our community is built upon respect, collaboration, and a passion for coding. To maintain a positive and inclusive environment, we have established the following Community Guidelines that all users are expected to adhere to:</p>
+
+            <h2 className="text-2xl font-bold mb-2">Respect Others</h2>
+            <p className="mb-4">Treat fellow users with respect and courtesy. Harassment, bullying, hate speech, discrimination, or any form of offensive behavior will not be tolerated. We encourage constructive criticism and open discussion while maintaining a respectful tone.</p>
+
+            <h2 className="text-2xl font-bold mb-2">Be Inclusive</h2>
+            <p className="mb-4">Our community is diverse and inclusive. Embrace different perspectives, experiences, and backgrounds. Avoid any actions or content that may alienate or exclude individuals based on race, ethnicity, gender, sexual orientation, religion, disability, or any other characteristic.</p>
+
+            <h2 className="text-2xl font-bold mb-2">Keep it Professional</h2>
+            <p className="mb-4">While participating in discussions or interactions, maintain a professional and appropriate demeanor. Refrain from using offensive language, posting explicit content, or engaging in disruptive behavior that may hinder the experience of others.</p>
+
+            <h2 className="text-2xl font-bold mb-2">Contribute Meaningfully</h2>
+            <p className="mb-4">Contribute positively to the community by sharing knowledge, offering assistance, and engaging in constructive conversations. Avoid spamming, self-promotion, or any activity that detracts from the primary purpose of the platform.</p>
+
+            <h2 className="text-2xl font-bold mb-2">Respect Privacy</h2>
+            <p className="mb-4">Respect the privacy of others. Refrain from sharing personal information or data without explicit consent. Additionally, do not attempt to access or misuse others&quot; accounts or data.</p>
+
+            <h2 className="text-2xl font-bold mb-2">Report Violations</h2>
+            <p className="mb-4">If you encounter any content or behavior that violates these Community Guidelines or raises concerns, please report it to our moderation team promptly. Your assistance helps maintain a safe and welcoming environment for everyone.</p>
+
+            <h2 className="text-2xl font-bold mb-2">Consequences of Violation</h2>
+            <p className="mb-4">Violations of these Community Guidelines may result in actions such as warning, temporary suspension, or permanent account termination, depending on the severity and frequency of the offense. Our moderation team will assess each situation diligently.</p>
+
+            <p className="mb-4">
+                <strong>Conclusion</strong>
+                <br />
+                By participating in our community, you agree to abide by these Community Guidelines. We encourage a collaborative and supportive atmosphere where everyone can learn and grow together.
+                <br />
+                Thank you for being a part of {"<BitWarriors/>"}!
+            </p>
+        </div>
+    );
+}
+
+export default CommunityGuidelines;
