6b26f273495ffe3100d107778129a3229339ed3a
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-12-03 23:17:15 +0530

    feat(page): add privacy policy

---
+++ src/app/(landing)/privacy-policy/page.tsx
@@ -0,0 +1,54 @@
+function PrivacyPolicy() {
+    return (
+        <div className="mx-auto max-w-5xl p-4">
+            <h1 className="text-4xl font-bold mb-4">Privacy Policy</h1>
+            <p className="mb-4">Effective Date: <strong className="font-bold">December 4th, 2023</strong></p>
+
+            <p className="mb-4">
+                Thank you for choosing to use <strong>{"<BitWarriors/>"}</strong> (&quot;the Platform&quot;). Your privacy and trust are of utmost importance to us. This Privacy Policy outlines the information we collect, how we use it, and the choices you have regarding your personal data when you use our services.
+            </p>
+
+            <h2 className="text-2xl font-bold mb-2">Information Collection and Use</h2>
+            <p className="mb-4">
+                When you register an account on the Platform, we collect certain information such as your username, email address, and password. You may also choose to sign in using third-party services like Github or Google, which will provide us with basic information from your profile, such as your name and email. Additionally, you have the option to add external links to your social media profiles (e.g., LinkedIn, personal website) in your user settings, but this information is entirely optional.
+            </p>
+            <p className="mb-4">
+                We use this information to create and manage your account, provide personalized experiences, and allow you to participate in coding challenges by submitting solutions.
+            </p>
+
+            <h2 className="text-2xl font-bold mb-2">Data Security</h2>
+            <p className="mb-4">
+                We prioritize the security of your personal information and employ industry-standard security measures to protect it against unauthorized access, disclosure, alteration, or destruction. However, no method of transmission over the internet or electronic storage is completely secure, so we cannot guarantee absolute security.
+            </p>
+
+            <h2 className="text-2xl font-bold mb-2">Google Analytics</h2>
+            <p className="mb-4">
+                We use Google Analytics, a web analytics service provided by Google Inc., to collect anonymous information about how users interact with the Platform. This data helps us understand user behavior and improve our services. Please note that this information is collected anonymously and is not linked to individual user accounts on our platform.
+            </p>
+
+            <h2 className="text-2xl font-bold mb-2">Account Deletion</h2>
+            <p className="mb-4">
+                You have the right to delete your account on the Platform. Please note that while your account may be deleted, your prior submissions or associated post/comment history may remain visible but anonymized (e.g., displayed as [deleted]). We may retain certain information as required by law or for legitimate business purposes.
+            </p>
+
+            <h2 className="text-2xl font-bold mb-2">Changes to This Privacy Policy</h2>
+            <p className="mb-4">
+                We reserve the right to update our Privacy Policy periodically. Any revisions will be effective immediately upon posting on this page. We encourage you to review this Privacy Policy regularly for any changes.
+            </p>
+
+            <p className="mb-4">
+                <strong>Your Consent</strong>
+                <br />
+                By using the Platform, you consent to our Privacy Policy and agree to the collection, use, and processing of your information as described herein.
+            </p>
+
+            <p className="mb-4">
+                <strong>Contact Us</strong>
+                <br />
+                If you have any questions or concerns regarding our Privacy Policy or the handling of your personal data, please contact us at {/* todo: replace me */}.
+            </p>
+        </div>
+    );
+}
+
+export default PrivacyPolicy;
