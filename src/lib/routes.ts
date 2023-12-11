d8bbe4f2ae60ece198ec798d182b58dc8e1173da
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-12-11 23:05:18 +0530

    fix(gateway): implemented api gateway

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,26 +1,26 @@
 export const API_ROUTES = {
 
     AUTH: {
-        GET: "http://localhost:3002/auth",
-        REGISTER_POST: "http://localhost:3002/register",
-        REGISTER_GITHUB_POST: "http://localhost:3002/oauth-github", 
-        LOGIN_POST: "http://localhost:3002/login",
-        LOGOUT_GET: "http://localhost:3002/logout",
-        MFA_POST: "http://localhost:3002/login-mfa",
+        GET: "http://localhost:3001/api/user/auth",
+        REGISTER_POST: "http://localhost:3001/api/user/register",
+        REGISTER_GITHUB_POST: "http://localhost:3001/api/user/oauth-github", 
+        LOGIN_POST: "http://localhost:3001/api/user/login",
+        LOGOUT_GET: "http://localhost:3001/api/user/logout",
+        MFA_POST: "http://localhost:3001/api/user/login-mfa",
     },
     VERIFICATION: {
-        EMAIL_GET: (verificationCode: string) => `http://localhost:3002/verify/${verificationCode}`,
+        EMAIL_GET: (verificationCode: string) => `http://localhost:3001/api/user/verify/${verificationCode}`,
         PWD_GET: (verificationCode: string, userId: string) => `/api/user/forget/${verificationCode}?uid=${userId}`
     },
     PROBLEMS: {
-        GET: "http://localhost:3003?limit=10",
-        PROBLEM_GET: (problemId: string) => `http://localhost:3003/${problemId}`,
-        RANDOM_GET: "http://localhost:3003/random"
+        GET: "http://localhost:3001/api/problem?limit=10",
+        PROBLEM_GET: (problemId: string) => `http://localhost:3001/api/problem/${problemId}`,
+        RANDOM_GET: "http://localhost:3001/api/problem/random"
     },
     PROFILE: {
-        GET: (username: string) => `http://localhost:3002/users/${username}`,
-        PUT: (username: string) => `http://localhost:3002/users/${username}`,
-        PASSWORD_PUT: (username: string) => `http://localhost:3002/users/${username}/password`
+        GET: (username: string) => `http://localhost:3001/api/user/users/${username}`,
+        PUT: (username: string) => `http://localhost:3001/api/user/users/${username}`,
+        PASSWORD_PUT: (username: string) => `http://localhost:3001/api/user/users/${username}/password`
     },
     PREFERENCES: {
         PATCH: '/api/user/prefs'
