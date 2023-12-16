abdf686834d6aa10f976428fcd7077e03ed2047d
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-12-16 22:17:05 +0530

    feat(problem): update style

--- src/app/(dashboard)/problems/[problem]/page.tsx
+++ src/app/(dashboard)/problems/[problem]/page.tsx
@@ -12,11 +12,12 @@ import { useState } from "react";
 import axios, { AxiosResponse } from "axios";
 import { API_ROUTES } from "@/lib/routes";
 import Terminal, { TerminalLine, TerminalLineType } from "@/components/Terminal";
-import { LuLoader2 } from "react-icons/lu";
+import { LuChevronDown, LuLoader2, LuTerminal } from "react-icons/lu";
 
 export interface TestResult {
     label: string;
     passed: boolean;
+    test: string; // add(1, 5) == 22
     message: string;
 }
 
@@ -28,11 +29,20 @@ export default function Page({ params }: { params: { problem: string } }) {
     ]);
 
     const [executing, setExecuting] = useState(false);
+    const [terminalVisible, setTerminalVisible] = useState(false);
+
+    function delay(s: number) {
+        return new Promise((res, rej) => {
+            return setTimeout(res, s);
+        })
+    }
 
     async function handleProblemRun() {
         if (!code) return;
-
+        setTerminalVisible(true);
+        setTerminalText([{ user: "user", content: "running test cases...", type: TerminalLineType.OUTPUT}]);
         setExecuting(true);
+        await delay(2000);
         const out = await axios.post(API_ROUTES.PROBLEMS.RUN_POST(params.problem), {
             code: code,
             language: "js"
@@ -50,38 +60,40 @@ export default function Page({ params }: { params: { problem: string } }) {
             }
         });
 
-        processedOutput.unshift({ user: "user", content: "running test cases...", type: TerminalLineType.OUTPUT})
+        
         if (!out.data.success) {
             processedOutput.push({ user: "user", content: out.data?.message || "an error occured.", type: TerminalLineType.ERROR})
         }
         processedOutput.push({ user: "user", content: "", type: TerminalLineType.COMMAND })
 
-        setTerminalText(processedOutput);
+        setTerminalText((existing) => [...existing, ...processedOutput]);
     }
 
     return (
         <>
-            <div className="space-y-1 p-10 pb-16 md:block">
-                <h1>{problem?.title ?? <Skeleton className="w-5" />}</h1>
-                <Badge>{problem?.difficulty ?? 'Unknown'}</Badge>
-                <div>Tabs</div>
+            <div className="space-y-1 p-5 pb-16 md:block">
                 <div className="grid grid-cols-3 gap-3">
-                    <div className="col-span-1 rounded-lg bg-neutral-900 h-[70vh] p-5">
+                    <div className="overflow-hidden word-break-all white-space-wrap col-span-1 rounded-lg bg-neutral-900 h-[80vh] p-5 ring-2 ring-inset ring-neutral-500">
                         <h1 className="text-2xl font-semibold">{problem?.title ?? <Skeleton className="w-5" />}</h1>
-                        <Markdown className="prose dark:prose-invert">{problem?.description ?? "*This problem is being loaded.*"}</Markdown>
-
+                        <Badge>{problem?.difficulty ?? 'Unknown'}</Badge>
+                        <Markdown className="max-h-[90%] overflow-y-scroll overflow-x-hidden word-break-all white-space-wrap mt-5 pb-3 prose dark:prose-invert">{problem?.description ?? "*This problem is being loaded.*"}</Markdown>
                     </div>
-                    <div className="col-span-2 flex flex-col gap-3 h-[70vh]">
-                        <div className="relative rounded-lg bg-neutral-900 font-mono h-[60%] p-2">
+                    <div className="col-span-2 flex flex-col gap-3 h-[80vh]">
+                        <div className="relative rounded-lg bg-neutral-900 font-mono min-h-[50%] h-full p-2 ring-2 ring-inset ring-neutral-500 hover:ring-amber-300 transition-shadow ease-in-out">
                             <CodeEditor code={code} setCode={setCode} />
                             <div className="flex gap-2 absolute bottom-4 right-4">
                                 <Button onClick={handleProblemRun} variant="ghost" disabled={executing}>{executing && <LuLoader2 className="animate-spin mr-2 h-4 w-4" /> }Run</Button>
                                 <Button variant="default" disabled={executing}>Submit</Button>
+                                { !terminalVisible && <Button onClick={() => setTerminalVisible(true)} variant="secondary"><LuTerminal/></Button>}
                             </div>
                         </div>
-                        <div className="rounded-lg bg-neutral-900 font-mono  h-[40%] p-5">
-                            <Terminal output={terminalText} />
-                        </div>
+                        { terminalVisible && 
+                            <div className="rounded-lg bg-neutral-900 font-mono ring-2 ring-inset ring-green-500 h-[50%] p-5 relative">
+                                <Button disabled={executing} onClick={() => setTerminalVisible(false)} className="absolute top-4 right-4" variant="secondary"><LuChevronDown/></Button>
+                                <h1 className="text-amber-500 font-semibold font-sans">• Terminal</h1>
+                                <Terminal output={terminalText} />
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
